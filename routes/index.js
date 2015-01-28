a3cd26defb1dcbb5eea8b6f32ce4da48b35ddc20
Stephen Woods <saw@stephenwoods.net>
2015-01-27 22:35:31 -0800

    flushhead

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,18 @@
 module.exports = function(router) {
 
 	router.get('/', function (req, res) {
-	  	res.render('index', {ctx:req.ctx});
+		res.render('index',{ctx:req.ctx}, function(err, html) {
+			// console.log('here', html);
+			setTimeout(function() {
+				res.write(html);
+				res.end();
+			},1000);
+
+		});
+	  	// res.render('index', {ctx:req.ctx});
 	});
 
 	router.get('/about', function (req, res) {
-		console.log('about');
 		res.render('about', {ctx:req.ctx});
 	})
 }
